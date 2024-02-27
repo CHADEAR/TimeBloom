6d0cdfdb17a1c4d4afab6c7b456f4088fe78f24c
Chadear <skyraindear@gmail.com>
2024-02-27 11:46:42 +0700

    dear
    music

---
+++ script.js
@@ -0,0 +1,71 @@
+const lofi = new Audio('/sound/lofi.mp3')
+const chill = new Audio('/sound/chill.mp3')
+const chill2 = new Audio('/sound/chill2.mp3')
+const miles = new Audio('/sound/miles.mp3')
+
+// selecting elements
+const prevBtn = document.querySelector('.previous');
+const playBtn = document.querySelector('.play-pause');
+const nextBtn = document.querySelector('.next');
+const songName = document.querySelector('.song-name');
+const playPauseIcon = document.querySelector('#play-pause-icon');
+
+
+const songs = [
+  { ele: lofi, audioName: 'lofi' },
+  { ele: chill, audioName: 'chill' },
+  { ele: chill2, audioName: 'chill2' },
+  { ele: miles, audioName: 'miles' },
+];
+
+for(const song of songs) {
+  song.ele.addEventListener('ended', ()=> {
+    updateSong('next');
+    playPauseSong();
+  })
+}
+
+let current = 0;
+let currentSong = songs[current].ele;
+songName.textContent = songs[current].audioName;
+
+playBtn.addEventListener('click',()=> {
+  playPauseSong();
+})
+
+nextBtn.addEventListener('click', () => {
+  updateSong('next');
+  playPauseSong();
+});
+
+prevBtn.addEventListener('click', () => {
+  updateSong('prev');
+  playPauseSong();
+});
+
+const updateSong = (action)=> {
+  currentSong.pause();
+  currentSong.currentTime = 0;
+
+  if(action === 'next'){
+    current++;
+    if(current > songs.length -1) current = 0;
+  }
+  if(action === 'prev'){
+    current--;
+    if(current < 0) current = songs.length - 1;
+  }
+  currentSong = songs[current].ele;
+  songName.textContent = songs[current].audioName;
+}
+
+const playPauseSong = ()=> {
+  if(currentSong.paused){
+    currentSong.play();
+    playPauseIcon.className = 'ph-bold ph-pause';
+  }
+  else {
+    currentSong.pause();
+    playPauseIcon.className = 'ph-bold ph-play';
+  }
+}
