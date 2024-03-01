8817a26168c88de70a70aa0f52bb1ab827539364
Jennie-ji <jjjenny7790@gmail.com>
2024-03-01 14:18:33 +0700

    register-login
    ทำตัว register-login แล้ว เหลือตกแต่ง
    ยังไม่ได้เพิ่มปุ่ม logout กับremember
    อัพขึ้นเพื่ออัพเดทข้อมูลให้เป็นโค้ดล่าสุด

--- firebase-TimeBloom/login_db.php
+++ firebase-TimeBloom/login_db.php
@@ -4,59 +4,47 @@
     require_once 'config/db.php';
 
     if (isset($_POST['login'])) {
-        $uname = $_POST['uname'];
+        $username = $_POST['username'];
         $password = $_POST['password'];
 
       
-        if (empty($uname)) {
-            $_SESSION['error'] = 'กรุณากรอกชื่อผู้ใช้';
-            header("location: login.php");
-        } 
-        else if (empty($password)) 
-        {
+        if (empty($username)) {
+            $_SESSION['error'] = 'กรุณากรอก username';
+            header("location: login-register.php");
+        } else if (empty($password)) {
             $_SESSION['error'] = 'กรุณากรอกรหัสผ่าน';
-            header("location: login.php");
-        } 
-        else 
-        {
-            try 
-            {
+            header("location: login-register.php");
+        } else if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
+            $_SESSION['error'] = 'รหัสผ่านต้องมีความยาวระหว่าง 5 ถึง 20 ตัวอักษร';
+            header("location: login-register.php");
+        } else {
+            try {
 
-                $check_data = $conn->prepare("SELECT * FROM users WHERE uname = :uname");
-                $check_data->bindParam(":uname", $uname);
+                $check_data = $conn->prepare("SELECT * FROM users WHERE username = :username");
+                $check_data->bindParam(":username", $username);
                 $check_data->execute();
                 $row = $check_data->fetch(PDO::FETCH_ASSOC);
 
-                if ($check_data->rowCount() > 0)
-                 {
-                    if ($uname == $row['uname'])
-                     {
-                        if (password_verify($password, $row['password']))
-                         {
-                                $_SESSION['admin_login'] = $row['id'];
-                                header("location: admin.php");
-                        } 
-                        else 
-                        {
+                if ($check_data->rowCount() > 0) {
+
+                    if ($username == $row['username']) {
+                        if (password_verify($password, $row['password'])) {
+                            $_SESSION['user_login'] = $row['id'];
+                                header("location: index.html");
+                        } else {
                             $_SESSION['error'] = 'รหัสผ่านผิด';
-                            header("location: login.php");
+                            header("location: login-register.php");
                         }
-                    } 
-                    else 
-                    {
-                        $_SESSION['error'] = 'ชื่อผู้ใช้ผิด';
-                        header("location: login.php");
+                    } else {
+                        $_SESSION['error'] = 'username ผิด';
+                        header("location: login-register.php");
                     }
-                } 
-                else 
-                {
+                } else {
                     $_SESSION['error'] = "ไม่มีข้อมูลในระบบ";
-                    header("location: login.php");
+                    header("location: login-register.php");
                 }
 
-            } 
-            catch(PDOException $e)
-            {
+            } catch(PDOException $e) {
                 echo $e->getMessage();
             }
         }
--- firebase-TimeBloom/register_db.php
+++ firebase-TimeBloom/register_db.php
@@ -3,26 +3,26 @@
     session_start();
     require_once 'config/db.php';
 
-    if (isset($_POST['signup'])) {
+    if (isset($_POST['regis'])) {
         $username = $_POST['username'];
         $email = $_POST['email'];
         $password = $_POST['password'];
 
-        if (empty($firstname)) {
+        if (empty($username)) {
             $_SESSION['error'] = 'กรุณากรอก username';
-            header("location: login-register.html");
+            header("location: index.html");
         } else if (empty($email)) {
             $_SESSION['error'] = 'กรุณากรอกอีเมล';
-            header("location: login-register.html");
+            header("location: login-register.php");
         } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $_SESSION['error'] = 'รูปแบบอีเมลไม่ถูกต้อง';
-            header("location: login-register.html");
+            header("location: login-register.php");
         } else if (empty($password)) {
             $_SESSION['error'] = 'กรุณากรอกรหัสผ่าน';
-            header("location: login-register.html");
+            header("location: login-register.php");
         } else if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
             $_SESSION['error'] = 'รหัสผ่านต้องมีความยาวระหว่าง 5 ถึง 20 ตัวอักษร';
-            header("location: login-register.html");
+            header("location: login-register.php");
         } else {
             try {
 
@@ -32,8 +32,8 @@
                 $row = $check_email->fetch(PDO::FETCH_ASSOC);
 
                 if ($row['email'] == $email) {
-                    $_SESSION['warning'] = "มีอีเมลนี้อยู่ในระบบแล้ว <a href='login-register.html'>คลิ๊กที่นี่</a> เพื่อเข้าสู่ระบบ";
-                    header("location: login-register.html");
+                    $_SESSION['warning'] = "มีอีเมลนี้อยู่ในระบบแล้ว <a href='login-register.php'>คลิ๊กที่นี่</a> เพื่อเข้าสู่ระบบ";
+                    header("location: login-register.php");
                 } else if (!isset($_SESSION['error'])) {
                     $passwordHash = password_hash($password, PASSWORD_DEFAULT);
                     $stmt = $conn->prepare("INSERT INTO users(username, email, password) 
@@ -42,11 +42,11 @@
                     $stmt->bindParam(":email", $email);
                     $stmt->bindParam(":password", $passwordHash);
                     $stmt->execute();
-                    $_SESSION['success'] = "สมัครสมาชิกเรียบร้อยแล้ว! <a href='login-register.html' class='alert-link'>คลิ๊กที่นี่</a> เพื่อเข้าสู่ระบบ";
-                    header("location: login-register.html");
+                    $_SESSION['success'] = "สมัครสมาชิกเรียบร้อยแล้ว! <a href='login-register.php' class='alert-link'>คลิ๊กที่นี่</a> เพื่อเข้าสู่ระบบ";
+                    header("location: login-register.php");
                 } else {
                     $_SESSION['error'] = "มีบางอย่างผิดพลาด";
-                    header("location: login-register.html");
+                    header("location: login-register.php");
                 }
 
             } catch(PDOException $e) {
