d6782472ed065b7fc899faf80bb51aae0c6ef347
Jennie-ji <jjjenny7790@gmail.com>
2024-03-03 16:11:42 +0700

    -
    -

--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
-    $servername = "sql313.infinityfree.com";
-    $username = "if0_36085009";
-    $password = "ctSPxpUZXtW";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
 
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=if0_36085009_users", $username, $password);
+        $conn = new PDO("mysql:host=$servername;dbname=register", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         // echo "Connected successfully";
